d6151720247042f2fca9f861c728ee45f58e30d4
Szymon Iwański <szymon.iwanski@hotmail.com>
2015-03-16 10:28:58 +0100

    Initial commit

---
+++ src/InputTest.java
@@ -0,0 +1,35 @@
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 
+ */
+
+/**
+ * @author Szymon Iwañski
+ *
+ */
+public class InputTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int c;
+		
+		try {
+			InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("test.txt")));
+			while((c = in.read()) >= 0) {
+				System.out.println((char)c);
+			}
+			in.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
