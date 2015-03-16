a3a68f9c09b01139bf91bb30200a620e254351c9
Szymon Iwański <szymon.iwanski@hotmail.com>
2015-03-16 13:06:52 +0100

    console out to file added

--- src/InputTest.java
+++ src/InputTest.java
@@ -1,7 +1,10 @@
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * 
@@ -13,6 +16,8 @@ import java.io.InputStream;
  */
 public class InputTest {
 
+	private static OutputStream out;
+
 	/**
 	 * @param args
 	 */
@@ -22,10 +27,13 @@ public class InputTest {
 		
 		try {
 			InputStream in = new LowerCaseInputStream(new BufferedInputStream(new FileInputStream("src/test.txt")));
+			out = new BufferedOutputStream(new FileOutputStream("src/result.txt"));
 			while((c = in.read()) >= 0) {
 				System.out.print((char)c);
+				out.write((char)c);
 			}
 			in.close();
+			out.close();
 		}
 		catch(IOException e) {
 			e.printStackTrace();
